65e90471046e32679e21dbbb847ab76ea486cbdb
Julien Nogueira <julien.nogueira19@gmail.com>
2017-06-28 02:45:54 +0200

    Adding optional tasks for the npm module
    Adding 2 optional functions
    Adding other unit tests with some error test cases
    Adding jsdoc
    Adding README

---
+++ backend/README.md
@@ -0,0 +1,45 @@
+gps-tracking
+=========
+
+A library allwoing to manipulate GPS tracks and coordinates to determine distances between them andlocation of GPS tracks.
+
+## Installation
+
+  npm install gps-tracking
+
+## Usage
+
+  var gpsTracker = require('gps-tracking'),
+      trackEntersBox = gpsTracker.trackEntersTheBox,
+      trackInBox = gpsTracker.trackIsInTheBox,
+      distTrackPoint = gpsTracker.getDistanceBetweenPointAndNearestGpsPoint,
+      distAtNearest = gpsTracker.getDistanceAtTheNearest,
+      timeAtNearest = gpsTracker.getTimeAtTheNearest;
+
+  var gpsTrack = [{lat : 32, lon : 22, time : 1484847603025},
+                  {lat : 25, lon : 28, time : 1484847607025}],
+      anotherGpsTrack = [{lat : 25, lon : 22, time : 1484847603025},
+                  {lat : 31, lon : 22, time : 1484847609025},
+                  {lat : 40, lon : 22, time : 1484847607025}],
+      pointNorthWest = {lat : 30, lon : 22},
+      pointSouthEast = {lat : 22, lon : 42},
+      ignoreTime = false;
+
+  var isEntering = trackEntersBox(gpsTrack, pointNorthWest, pointSouthEast);
+  var isInside = trackInBox(gpsTrack, pointNorthWest, pointSouthEast);
+  var distance = distTrackPoint(gpsTrack, gpsPoint);
+  var distance = distAtNearest(gpsTrack, anotherGpsTrack, ignoreTime);
+  var timestamp = timeAtNearest(gpsTrack, anotherGpsTrack);
+
+## Tests
+
+  npm test
+
+## Contributing
+
+Take care to maintain the existing coding style.
+Add unit tests for any new or changed functionality.
+
+## Release History
+
+* 0.0.1 Initial release
--- backend/test/test.js
+++ backend/test/test.js
@@ -3,7 +3,9 @@ var should = require('chai').should(),
     gpsTracker = require('../index'),
     trackEntersBox = gpsTracker.trackEntersTheBox,
     trackInBox = gpsTracker.trackIsInTheBox,
-    distTrackPoint = gpsTracker.getDistanceBetweenPointAndNearestGpsPoint;
+    distTrackPoint = gpsTracker.getDistanceBetweenPointAndNearestGpsPoint,
+    distAtNearest = gpsTracker.getDistanceAtTheNearest,
+    timeAtNearest = gpsTracker.getTimeAtTheNearest;
 
 describe('#trackEntersBox', function() {
     it('verify track enters the box', function() {
@@ -12,7 +14,27 @@ describe('#trackEntersBox', function() {
             pointNorthWest = {lat : 30, lon : 22},
             pointSouthEast = {lat : 22, lon : 42};
 
-        trackEntersBox(gpsTrack, pointNorthWest, pointSouthEast).should.equal(1);
+        trackEntersBox(gpsTrack, pointNorthWest, pointSouthEast).should.equal(true);
+    });
+    it('verify track does not enter the box', function() {
+        var gpsTrack = [{lat : 32, lon : 22, time : 1484847603025},
+                        {lat : 21, lon : 28, time : 1484847607025}],
+            pointNorthWest = {lat : 30, lon : 22},
+            pointSouthEast = {lat : 22, lon : 42};
+
+        trackEntersBox(gpsTrack, pointNorthWest, pointSouthEast).should.equal(false);
+    });
+    it('verify TypeError thrown when wrong params', function() {
+        var badfn = function() {
+            var gpsTrack = [{lat : 32, lon : 22, time : 1484847603025},
+                            {lat : 42, lon : 22, time : 1484847607025}],
+                pointNorthWest = {lat : 32, lon : 22},
+                pointSouthEast = "wrong";
+
+            trackEntersBox(gpsTrack, pointNorthWest, pointSouthEast);
+        }
+        
+        expect(badfn).to.throw(TypeError);
     });
 });
 
@@ -23,7 +45,19 @@ describe('#trackInBox', function() {
             pointNorthWest = {lat : 32, lon : 22},
             pointSouthEast = {lat : 22, lon : 42};
 
-        trackInBox(gpsTrack, pointNorthWest, pointSouthEast).should.equal(1);
+        trackInBox(gpsTrack, pointNorthWest, pointSouthEast).should.equal(true);
+    });
+    it('verify TypeError thrown when wrong params', function() {
+        var badfn = function() {
+            var gpsTrack = [{lat : 32, lon : 22, time : 1484847603025},
+                            {lat : 42, lon : 22, time : 1484847607025}],
+                pointNorthWest = {lat : 32, la : 22},
+                pointSouthEast = {lat : 22, lon : 42};
+
+            trackInBox(gpsTrack, pointNorthWest, pointSouthEast);
+        }
+        
+        expect(badfn).to.throw(TypeError);
     });
 });
 
@@ -33,7 +67,78 @@ describe('#distTrackPoint', function() {
                         {lat : 42, lon : 22, time : 1484847607025}],
             gpsPoint = {lat : 32.8994, lon : 22};
 
-        //distTrackPoint(gpsTrack, gpsPoint).should.equal(100);
         expect(distTrackPoint(gpsTrack, gpsPoint)).to.be.within(99.99, 100.01);
     });
+    it('verify TypeError thrown when wrong params', function() {
+        var badfn = function() {
+            var gpsTrack = [{lat : 32, lon : 22, time : 1484847603025},
+                            {lat : 42, lon : 22, time : 1484847607025}],
+                gpsPoint = {lat : 32.8994, lon : 22};
+
+            distTrackPoint(gpsPoint, gpsTrack);
+        }
+        
+        expect(badfn).to.throw(TypeError);
+    });
 });
+
+describe('#distAtNearest', function() {
+    it('verify shortest distance is 111km when ignoring time', function() {
+        var gpsTrack = [{lat : 32, lon : 22, time : 1484847603025},
+                        {lat : 44, lon : 22, time : 1484847609025},
+                        {lat : 42, lon : 22, time : 1484847607025}],
+            anotherGpsTrack = [{lat : 25, lon : 22, time : 1484847603025},
+                        {lat : 31, lon : 22, time : 1484847609025},
+                        {lat : 40, lon : 22, time : 1484847607025}];
+
+        expect(distAtNearest(gpsTrack, anotherGpsTrack, true)).to.be.within(111, 112);
+    });
+    it('verify shortest distance is 222km when same time', function() {
+        var gpsTrack = [{lat : 32, lon : 22, time : 1484847603025},
+                        {lat : 44, lon : 22, time : 1484847609025},
+                        {lat : 42, lon : 22, time : 1484847607025}],
+            anotherGpsTrack = [{lat : 25, lon : 22, time : 1484847603025},
+                        {lat : 31, lon : 22, time : 1484847609025},
+                        {lat : 40, lon : 22, time : 1484847607025}];
+
+        expect(distAtNearest(gpsTrack, anotherGpsTrack, false)).to.be.within(221, 223);
+    });
+    it('verify TypeError thrown when wrong params', function() {
+        var badfn = function() {
+            var gpsTrack = [{lat : 32, lon : 22, time : 1484847603025},
+                            {lat : 44, lon : 22, time : 1484847609025},
+                            {lat : 42, lon : 22}],
+                anotherGpsTrack = [{lat : 25, lon : 22, time : 1484847603025},
+                            {lat : 31, lon : 22, time : 1484847609025},
+                            {lat : 40, lon : 22, time : 1484847607025}];
+
+            distAtNearest(gpsTrack, anotherGpsTrack);
+        }
+        
+        expect(badfn).to.throw(TypeError);
+    });
+});
+
+describe('#timeAtNearest', function() {
+    it('verify shortest distance is at time 1484847607025', function() {
+        var gpsTrack = [{lat : 32, lon : 22, time : 1484847603025},
+                        {lat : 44, lon : 22, time : 1484847609025},
+                        {lat : 42, lon : 22, time : 1484847607025}],
+            anotherGpsTrack = [{lat : 25, lon : 22, time : 1484847603025},
+                        {lat : 31, lon : 22, time : 1484847609025},
+                        {lat : 40, lon : 22, time : 1484847607025}];
+
+        timeAtNearest(gpsTrack, anotherGpsTrack).should.equal(1484847607025);
+    });
+    it('verify TypeError thrown when wrong params', function() {
+        var badfn = function() {
+            var gpsTrack = [{lat : 32, lon : 22, time : 1484847603025},
+                            {lat : 25, lon : 28, time : 1484847607025}];
+
+            timeAtNearest(gpsTrack);
+        }
+        
+        expect(badfn).to.throw(TypeError);
+    });
+});
+
